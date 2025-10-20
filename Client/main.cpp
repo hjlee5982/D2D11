fe52ead048fd87c35bfbd11f0c7b337d304ea705
hjlee5982 <hjlee5982@naver.com>
2025-10-20 14:34:19 +0900

    Initial Commit

---
+++ Client/main.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+#ifdef _DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+
+	return 0;
+}
---
+++ Libraries/Include/Engine/pch.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+/* WinAPI Çì´õ */
+#include<Windows.h>
+#include<assert.h>
+#include<optional>
+#include<comdef.h>
